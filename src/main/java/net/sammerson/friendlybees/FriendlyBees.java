5615c7e4a7ff0a456da4dcfbe069c9233257c35b
Sammersonnn <Sammersonnn@gmail.com>
2022-06-17 02:24:54 -0400

    init commit

---
+++ src/main/java/net/sammerson/friendlybees/FriendlyBees.java
@@ -0,0 +1,17 @@
+package net.sammerson.friendlybees;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FriendlyBees implements ModInitializer {
+
+	public static final String MOD_ID = "friendlybees";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+	}
+}
---
+++ src/main/java/net/sammerson/friendlybees/FriendlyBeesClient.java
@@ -0,0 +1,10 @@
+package net.sammerson.friendlybees;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FriendlyBeesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
